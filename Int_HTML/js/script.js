61674d80d2d502c1ff1e80baf4736b8d7663cf33
vilmarjet <32167312+vilmarjet@users.noreply.github.com>
2018-01-28 22:53:56 -0500

    Add files via upload

---
+++ Int_HTML/js/script.js
@@ -0,0 +1,10 @@
+function startLogic() {
+    var newHtml = '<em>New Header</em>';
+    document.getElementById('headerItem').innerHTML = newHtml;
+}
+
+
+function clickHandler() {
+  alert('You clicked me!');
+  greeting = "Have a good weekend!";
+}
